0b798299c32dfba73a0cdf42007bc2725d3a2cec
RFullum <rfullum@gmail.com>
2026-05-29 07:32:16 -0400

    add Color Palette

---
+++ Source/GUI/GUIUtils/ColorPalette.h
@@ -0,0 +1,30 @@
+#include <JuceHeader.h>
+
+//==============================================================================
+
+namespace Palette
+{
+
+    struct Theme
+    {
+        juce::Colour background;
+        juce::Colour structure;
+        juce::Colour primaryAccent;
+        juce::Colour secondaryAccent;
+        juce::Colour pinkAccent;
+        juce::Colour textPrimary;
+        juce::Colour textSecondary;
+    };
+
+    inline const Theme DefaultTheme
+    {
+        juce::Colour(0xFF0D0E12),   // background:      not-quite-black
+        juce::Colour(0xFF1A1C24),   // structure:       dark blue-grey
+        juce::Colour(0xFFFF6B00),   // primaryAccent:   blaze orange
+        juce::Colour(0xFF00C8B4),   // secondaryAccent: teal
+        juce::Colour(0xFFFF2D78),   // pinkAccent:      hot pink
+        juce::Colour(0xFFD0D8E8),   // textPrimary:     blue-tinted off-white
+        juce::Colour(0xFF5A6480)    // textSecondary:   blue-gray
+    }
+
+}   // namespace Palette
