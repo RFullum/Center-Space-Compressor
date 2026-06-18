b2a49766a1f34cc3861a809fbc79ef2a7a8aa821
RFullum <rfullum@gmail.com>
2026-06-18 13:47:13 -0400

    migrate ColorPalette to FullumModules

--- Source/GUI/GUIUtils/ColorPalette.h
+++ Source/GUI/GUIUtils/ColorPalette.h
@@ -10,22 +10,12 @@
 
 #pragma once
 
-#include <JuceHeader.h>
+#include "GUI/ColorPalette.h"
 
 //==============================================================================
 
 namespace Palette
 {
-
-    struct Theme
-    {
-        juce::Colour background;
-        juce::Colour structure;
-        juce::Colour primaryAccent;
-        juce::Colour secondaryAccent;
-        juce::Colour textPrimary;
-        juce::Colour textSecondary;
-    };
 
     inline const Theme DefaultTheme
     {
@@ -34,7 +24,9 @@ namespace Palette
         juce::Colour(0xFFFF6B00),   // primaryAccent:   blaze orange
         juce::Colour(0xFF00C8B4),   // secondaryAccent: teal
         juce::Colour(0xFFD0D8E8),   // textPrimary:     blue-tinted off-white
-        juce::Colour(0xFF5A6480)    // textSecondary:   blue-gray
+        juce::Colour(0xFF5A6480),   // textSecondary:   blue-gray
+        juce::Colour(0xFFFF2D78),   // pinkAccent:      hot pink
+        juce::Colour(0xFFFF6B00)    // orangeAccent:    blaze orange
     };
 
 }   // namespace Palette
